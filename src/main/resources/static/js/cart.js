ab4650ff1e54850154c98f1731e571e8cb0f8289
flouritess <302618914@qq.com>
2020-01-29 23:15:12 +0800

    购物车

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -31,13 +31,36 @@ $(document).ready(function () {
 	});
 
 	// 所有复选(:checkbox)框点击事件
-	$(".gwc_tb2 input[name=newslist]").click(function () {
-		if ($(this).attr("checked")) {
+	// $(".gwc_tb2 input[name=newslist]").click(function () {
+	// 	if ($(this).attr("checked")) {
+    //
+	// 	} else {
+    //
+	// 	}
+	// });
+    $(".gwc_tb2 input[name=newslist]").click(function () {
+        var length=$("#cart").find("table").length ;
+        var totalprice=0;
+        var count=0;
+        for(var i=1;i<length-1;i++){
+            if($("#newslist"+i).prop('checked')){
+                count=count+1;
+                var number=$("#text_box"+i).val();
+                var price=$("#total"+i).html().trim();
+                var onetotalprice=number*price;
+                totalprice=totalprice+onetotalprice;
+            }
+        }
+        console.log("当前总数量:"+count);
+        console.log("当前总价钱:"+totalprice);
 
-		} else {
+        var label=document.getElementById("zong1");
+        label.innerText=totalprice;
+        var label2=document.getElementById("shuliang");
+        console.log(label2);
+        label2.innerText=count;
 
-		}
-	});
+    });
 
 	// 输出
 	$(".gwc_tb2 input[name=newslist]").click(function () {
@@ -58,7 +81,7 @@ function GetCount() {
 		}
 	});
 	$("#shuliang").text(aa);
-	$("#zong1").html((conts).toFixed(2));
+	// $("#zong1").html((conts).toFixed(2));
 	$("#jz1").css("display", "none");
 	$("#jz2").css("display", "block");
 }
@@ -83,10 +106,12 @@ function GetCount() {
 		$("#add2").click(function () {
 			t.val(parseInt(t.val()) + 1)
 			// setTotal(); GetCount();
+            setTotal2();
 		})
 		$("#min2").click(function () {
 			t.val(parseInt(t.val(1)) - 1)
 			t.val(1)//初始值防止为负数ADD deathghost
+            setTotal2();
 			// setTotal(); GetCount();
 		})
 		// function setTotal() {
@@ -99,11 +124,13 @@ function GetCount() {
 		var t = $("#text_box1");
 		$("#add1").click(function () {
 			t.val(parseInt(t.val()) + 1)
+            setTotal2();
 			// setTotal(); GetCount();
 		})
 		$("#min1").click(function () {
 			t.val(parseInt(t.val()) - 1)
 			t.val(1)//初始值防止为负数ADD deathghost
+            setTotal2();
 			// setTotal(); GetCount();
 		})
 		// function setTotal() {
@@ -117,20 +144,24 @@ $(function () {
     var t = $("#text_box3");
     $("#add3").click(function () {
         t.val(parseInt(t.val()) + 1)
+        setTotal2();
     })
     $("#min3").click(function () {
         t.val(parseInt(t.val()) - 1)
         t.val(1)
+        setTotal2();
     })
 })
 $(function () {
     var t = $("#text_box4");
     $("#add4").click(function () {
         t.val(parseInt(t.val()) + 1)
+        setTotal2();
     })
     $("#min4").click(function () {
         t.val(parseInt(t.val()) - 1)
         t.val(1)
+        setTotal2();
     })
 })
 // <!---总数---->
@@ -147,63 +178,93 @@ $(function () {
 
 			}
 			//alert(len.length);
-			$("#zong1").text(parseInt(num).toFixed(2));
+			// $("#zong1").text(parseInt(num).toFixed(2));
 			$("#shuliang").text(len.length);
 		}
 		//setTotal();
 	})
+function setTotal2(){
+    var length=$("#cart").find("table").length ;
+    var totalprice=0;
+    for(var i=1;i<length-1;i++){
+        if($("#newslist"+i).prop('checked')){
+            var number=$("#text_box"+i).val();
+            var price=$("#total"+i).html().trim();
+            var onetotalprice=number*price;
+            totalprice=totalprice+onetotalprice;
+        }
+    }
+    console.log("当前总价钱:"+totalprice);zong1
+    var label=document.getElementById("zong1");
+    label.innerText=totalprice;
+    // var value=$("#zong1").html();
+    // value=totalprice;
+}
+
+//计算总价钱
+// $(function () {
+//     var length=$("#cart").find("table").length ;
+//     var totalprice=0;
+//     for(var i=1;i<length-1;i++){
+//         var number=$("#text_box"+i).val();
+//         var price=$("#total"+i).html().trim();
+//         var onetotalprice=number*price;
+//         totalprice=totalprice+onetotalprice;
+//     }
+//     console.log("当前总价钱:"+totalprice);
+// })
 //add to cart shoppage
-var data = {"total":0,"rows":[]};
-		var totalCost = 0;
-		
-		$(function(){
-			$('#cartcontent').datagrid({
-				singleSelect:true
-			});
-			$('.item').draggable({
-				revert:true,
-				proxy:'clone',
-				onStartDrag:function(){
-					$(this).draggable('options').cursor = 'not-allowed';
-					$(this).draggable('proxy').css('z-index',2);
-				},
-				onStopDrag:function(){
-					$(this).draggable('options').cursor='move';
-				}
-			});
-			$('.cart').droppable({
-				onDragEnter:function(e,source){
-					$(source).draggable('options').cursor='auto';
-				},
-				onDragLeave:function(e,source){
-					$(source).draggable('options').cursor='not-allowed';
-				},
-				onDrop:function(e,source){
-					var name = $(source).find('p:eq(0)').html();
-					var price = $(source).find('p:eq(1)').html();
-					addProduct(name, parseFloat(price.split('￥')[1]));
-				}
-			});
-		});
-		
-		function addProduct(name,price){
-			function add(){
-				for(var i=0; i<data.total; i++){
-					var row = data.rows[i];
-					if (row.name == name){
-						row.quantity += 1;
-						return;
-					}
-				}
-				data.total += 1;
-				data.rows.push({
-					name:name,
-					quantity:1,
-					price:price
-				});
-			}
-			add();
-			totalCost += price;
-			$('#cartcontent').datagrid('loadData', data);
-			$('div.cart .total').html('共计金额: ￥'+totalCost);
-		}
+// var data = {"total":0,"rows":[]};
+// 		var totalCost = 0;
+//
+// 		$(function(){
+// 			$('#cartcontent').datagrid({
+// 				singleSelect:true
+// 			});
+// 			$('.item').draggable({
+// 				revert:true,
+// 				proxy:'clone',
+// 				onStartDrag:function(){
+// 					$(this).draggable('options').cursor = 'not-allowed';
+// 					$(this).draggable('proxy').css('z-index',2);
+// 				},
+// 				onStopDrag:function(){
+// 					$(this).draggable('options').cursor='move';
+// 				}
+// 			});
+// 			$('.cart').droppable({
+// 				onDragEnter:function(e,source){
+// 					$(source).draggable('options').cursor='auto';
+// 				},
+// 				onDragLeave:function(e,source){
+// 					$(source).draggable('options').cursor='not-allowed';
+// 				},
+// 				onDrop:function(e,source){
+// 					var name = $(source).find('p:eq(0)').html();
+// 					var price = $(source).find('p:eq(1)').html();
+// 					addProduct(name, parseFloat(price.split('￥')[1]));
+// 				}
+// 			});
+// 		});
+//
+// 		function addProduct(name,price){
+// 			function add(){
+// 				for(var i=0; i<data.total; i++){
+// 					var row = data.rows[i];
+// 					if (row.name == name){
+// 						row.quantity += 1;
+// 						return;
+// 					}
+// 				}
+// 				data.total += 1;
+// 				data.rows.push({
+// 					name:name,
+// 					quantity:1,
+// 					price:price
+// 				});
+// 			}
+// 			add();
+// 			totalCost += price;
+// 			$('#cartcontent').datagrid('loadData', data);
+// 			$('div.cart .total').html('共计金额: ￥'+totalCost);
+// 		}
