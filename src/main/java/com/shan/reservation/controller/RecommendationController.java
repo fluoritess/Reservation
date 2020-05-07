a7c94e99b131414b29055c57a7d2ea1ea48ba065
flouritess <302618914@qq.com>
2020-05-07 22:27:38 +0800

    推荐算法调优

--- src/main/java/com/shan/reservation/controller/RecommendationController.java
+++ src/main/java/com/shan/reservation/controller/RecommendationController.java
@@ -36,6 +36,8 @@ public class RecommendationController {
     RestaurantService RestaurantService;
     @Autowired
     com.shan.reservation.service.CollectService CollectService;
+    @Autowired
+    UserService UserService;
     @ResponseBody
     @RequestMapping("/Pearson" )
     @ArchivesLog(operationType = "推荐", operationName = "使用皮尔逊推荐算法")
@@ -43,50 +45,66 @@ public class RecommendationController {
        Integer id=Integer.parseInt(map.get("user_id"));
 //       String orderno=map.get("orderno");
 //       order oreder=OrderService.selectOrder(orderno);
+        //-----------------最终评分---------------------------
+        double tar_score=0;
+        //-----------------最终评分用户id---------------------------
+        int tar_id=0;
         //-----------------用户评价列表---------------------------
        List<evaluation> evaluations=EvaluationService.selectByuserId(id);
-       List<evaluation> evaluations2=EvaluationService.selectByuserId(1);
-        //-----------------统一长度------------------------------
-       int size=0;
-        if(evaluations.size()<evaluations2.size()){
-           size=evaluations.size();
+       List<user> userList=UserService.selectAllUser();
+       int n=userList.size();
+       for(int i=0;i<n;i++){
+           user user=userList.get(i);
+           int user_id=user.getUserId();
+           List<evaluation> evaluations2=EvaluationService.selectByuserId(user_id);
+           //-----------------统一长度------------------------------
+           int size=0;
+           if(evaluations.size()<evaluations2.size()){
+               size=evaluations.size();
+           }
+           else{
+               size=evaluations2.size();
+           }
+           //------------------------获取评分项------------------------
+           double[] arr=new double[size];
+           double[] arr2=new double[size];
+           Iterator it=evaluations.iterator();
+           int count=0;
+           int count2=0;
+           while(it.hasNext()&&count<size){
+               evaluation evaluation=(evaluation)it.next();
+               double score=evaluation.getScore();
+               int re_id=evaluation.getRestaurantId();
+               Iterator it2=evaluations2.iterator();
+               while(it2.hasNext()){
+                   evaluation evaluation_=(evaluation)it2.next();
+                   double score_=evaluation_.getScore();
+                   int re_id2=evaluation_.getRestaurantId();
+                   if(re_id==re_id2){
+                       arr[count]=score;
+                       arr2[count2]=score_;
+                       count2++;
+                       break;
+                   }
+               }
+               count++;
+               //-------------------------------count2<count是因为该栏本两用户没有进行共同评价，为了使评分矩阵规整进行修正--------------------------------
+               if(count2<count){
+                   count--;
+               }
+           }
+           //-------------------------------评分--------------------------------
+           double score=PearsonService.CalculationPearson(arr,arr2);
+           //-------------------------------判断与最终评分的大小--------------------------------
+           if(score>tar_score){
+               tar_score=score;
+               tar_id=user_id;
+           }
        }
-       else{
-           size=evaluations2.size();
-       }
-       //------------------------生成推荐列表------------------------
-        double[] arr=new double[size];
-        double[] arr2=new double[size];
-        Iterator it=evaluations.iterator();
-        int count=0;
-        int count2=0;
-        while(it.hasNext()&&count<size){
-            evaluation evaluation=(evaluation)it.next();
-            double score=evaluation.getScore();
-            int re_id=evaluation.getRestaurantId();
-            Iterator it2=evaluations2.iterator();
-            while(it2.hasNext()){
-                evaluation evaluation_=(evaluation)it2.next();
-                double score_=evaluation_.getScore();
-                int re_id2=evaluation_.getRestaurantId();
-                if(re_id==re_id2){
-                    arr[count]=score;
-                    arr2[count2]=score_;
-                    count2++;
-                    break;
-                }
-            }
-            count++;
-            //-------------------------------count2<count是因为该栏本两用户没有进行共同评价，为了使评分矩阵规整进行修正--------------------------------
-            if(count2<count){
-                count--;
-            }
-        }
-        //-------------------------------评分--------------------------------
-        double score=PearsonService.CalculationPearson(arr,arr2);
-        //-------------------------------推荐--------------------------------
-        if(score>=0.4){
-            List<collectUtil> list=CollectService.selectByUser(1);
+
+        //-------------------------------生成推荐列表--------------------------------
+        if(tar_score>=0.4){
+            List<collectUtil> list=CollectService.selectByUser(tar_id);
             List<restaurantUtil> restaurantUtil=new ArrayList<>();
             for(collectUtil c:list){
                 int re_id=c.getRestaurantId();
